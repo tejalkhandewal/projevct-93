9c2d120823a4546abb351129cbb6aba7d7760b9b
tejalkhandewal <93588531+tejalkhandewal@users.noreply.github.com>
2022-06-04 15:04:01 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDRKazaIcO3NDC0LS9nshXAO4DP4vcBudA",
+    authDomain: "project93-3b7b8.firebaseapp.com",
+    databaseURL: "https://project93-3b7b8-default-rtdb.firebaseio.com",
+    projectId: "project93-3b7b8",
+    storageBucket: "project93-3b7b8.appspot.com",
+    messagingSenderId: "110645501280",
+    appId: "1:110645501280:web:43b10f7dcfe586792ad205"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
